b0c9ecef55700853cfceb6150ab373fea315cfc0
bhavesh <bhaveshgupta41@gmail.com>
2023-04-28 11:48:56 +0530

    react project

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,17 +1,25 @@
-import React from "react";
-import { PRODUCTS } from "../../products";
+import React, { useEffect, useState } from 'react';
 import { Product } from "./product";
 import "./shop.css";
 
 export const Shop = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/Products')
+      .then(response => response.json())
+      .then(data => setData(data));
+      
+  }, []);
   return (
+   
     <div className="shop">
       <div className="shopTitle">
-        <h1>PedroTech Shop</h1>
+        <h4>My Shop</h4>
       </div>
 
       <div className="products">
-        {PRODUCTS.map((product) => (
+        {data.map((product) => (
           <Product data={product} />
         ))}
       </div>
